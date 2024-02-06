0ef448490b02add7a0849acc50315a283521aa02
Pedro Euzebio <pedroeuzebio.contato@gmail.com>
2024-02-06 12:46:47 -0300

    feat: add global metadata in BaseHead component and config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Manuel Ernesto Garcia | Website';
-export const SITE_DESCRIPTION = 'Software Engineer from Cuba • JS • Minimalist • Creando soluciones y compartiendo lo que aprendo.';
+export const SITE_TITLE = 'Pedro Euzebio | Desenvolvedor Web';
+export const SITE_DESCRIPTION = 'Olá, tudo bem? Eu sou o Pedro Euzebio, desenvolvedor web. Explore meu portfólio e conheça sobre mi, minha stack, meus projetos e como entrar em contato.';

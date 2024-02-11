199484ce9da81044ff4ffc12e3661d098d6ac0f9
Pedro Euzebio <pedroeuzebio.contato@gmail.com>
2024-02-11 11:27:49 -0300

    feat: add text period in experience and education, certification and update text idiomas

--- .astro/types.d.ts
+++ .astro/types.d.ts
@@ -1,14 +1,5 @@
 declare module 'astro:content' {
 	interface Render {
-		'.mdx': Promise<{
-			Content: import('astro').MarkdownInstance<{}>['Content'];
-			headings: import('astro').MarkdownHeading[];
-			remarkPluginFrontmatter: Record<string, any>;
-		}>;
-	}
-}
-declare module 'astro:content' {
-	interface Render {
 		'.md': Promise<{
 			Content: import('astro').MarkdownInstance<{}>['Content'];
 			headings: import('astro').MarkdownHeading[];
@@ -19,8 +10,9 @@ declare module 'astro:content' {
 
 declare module 'astro:content' {
 	export { z } from 'astro/zod';
-	export type CollectionEntry<C extends keyof typeof entryMap> =
-		(typeof entryMap)[C][keyof (typeof entryMap)[C]];
+
+	type Flatten<T> = T extends { [K: string]: infer U } ? U : never;
+	export type CollectionEntry<C extends keyof AnyEntryMap> = Flatten<AnyEntryMap[C]>;
 
 	// TODO: Remove this when having this fallback is no longer relevant. 2.3? 3.0? - erika, 2023-04-04
 	/**
@@ -61,12 +53,9 @@ declare module 'astro:content' {
 
 	type BaseSchemaWithoutEffects =
 		| import('astro/zod').AnyZodObject
-		| import('astro/zod').ZodUnion<import('astro/zod').AnyZodObject[]>
+		| import('astro/zod').ZodUnion<[BaseSchemaWithoutEffects, ...BaseSchemaWithoutEffects[]]>
 		| import('astro/zod').ZodDiscriminatedUnion<string, import('astro/zod').AnyZodObject[]>
-		| import('astro/zod').ZodIntersection<
-				import('astro/zod').AnyZodObject,
-				import('astro/zod').AnyZodObject
-		  >;
+		| import('astro/zod').ZodIntersection<BaseSchemaWithoutEffects, BaseSchemaWithoutEffects>;
 
 	type BaseSchema =
 		| BaseSchemaWithoutEffects
@@ -74,223 +63,138 @@ declare module 'astro:content' {
 
 	export type SchemaContext = { image: ImageFunction };
 
-	type BaseCollectionConfig<S extends BaseSchema> = {
+	type DataCollectionConfig<S extends BaseSchema> = {
+		type: 'data';
 		schema?: S | ((context: SchemaContext) => S);
 	};
-	export function defineCollection<S extends BaseSchema>(
-		input: BaseCollectionConfig<S>
-	): BaseCollectionConfig<S>;
 
-	type EntryMapKeys = keyof typeof entryMap;
+	type ContentCollectionConfig<S extends BaseSchema> = {
+		type?: 'content';
+		schema?: S | ((context: SchemaContext) => S);
+	};
+
+	type CollectionConfig<S> = ContentCollectionConfig<S> | DataCollectionConfig<S>;
+
+	export function defineCollection<S extends BaseSchema>(
+		input: CollectionConfig<S>
+	): CollectionConfig<S>;
+
 	type AllValuesOf<T> = T extends any ? T[keyof T] : never;
-	type ValidEntrySlug<C extends EntryMapKeys> = AllValuesOf<(typeof entryMap)[C]>['slug'];
+	type ValidContentEntrySlug<C extends keyof ContentEntryMap> = AllValuesOf<
+		ContentEntryMap[C]
+	>['slug'];
 
 	export function getEntryBySlug<
-		C extends keyof typeof entryMap,
-		E extends ValidEntrySlug<C> | (string & {})
+		C extends keyof ContentEntryMap,
+		E extends ValidContentEntrySlug<C> | (string & {})
 	>(
 		collection: C,
 		// Note that this has to accept a regular string too, for SSR
 		entrySlug: E
-	): E extends ValidEntrySlug<C>
+	): E extends ValidContentEntrySlug<C>
 		? Promise<CollectionEntry<C>>
 		: Promise<CollectionEntry<C> | undefined>;
-	export function getCollection<C extends keyof typeof entryMap, E extends CollectionEntry<C>>(
+
+	export function getDataEntryById<C extends keyof DataEntryMap, E extends keyof DataEntryMap[C]>(
+		collection: C,
+		entryId: E
+	): Promise<CollectionEntry<C>>;
+
+	export function getCollection<C extends keyof AnyEntryMap, E extends CollectionEntry<C>>(
 		collection: C,
 		filter?: (entry: CollectionEntry<C>) => entry is E
 	): Promise<E[]>;
-	export function getCollection<C extends keyof typeof entryMap>(
+	export function getCollection<C extends keyof AnyEntryMap>(
 		collection: C,
 		filter?: (entry: CollectionEntry<C>) => unknown
 	): Promise<CollectionEntry<C>[]>;
 
+	export function getEntry<
+		C extends keyof ContentEntryMap,
+		E extends ValidContentEntrySlug<C> | (string & {})
+	>(entry: {
+		collection: C;
+		slug: E;
+	}): E extends ValidContentEntrySlug<C>
+		? Promise<CollectionEntry<C>>
+		: Promise<CollectionEntry<C> | undefined>;
+	export function getEntry<
+		C extends keyof DataEntryMap,
+		E extends keyof DataEntryMap[C] | (string & {})
+	>(entry: {
+		collection: C;
+		id: E;
+	}): E extends keyof DataEntryMap[C]
+		? Promise<DataEntryMap[C][E]>
+		: Promise<CollectionEntry<C> | undefined>;
+	export function getEntry<
+		C extends keyof ContentEntryMap,
+		E extends ValidContentEntrySlug<C> | (string & {})
+	>(
+		collection: C,
+		slug: E
+	): E extends ValidContentEntrySlug<C>
+		? Promise<CollectionEntry<C>>
+		: Promise<CollectionEntry<C> | undefined>;
+	export function getEntry<
+		C extends keyof DataEntryMap,
+		E extends keyof DataEntryMap[C] | (string & {})
+	>(
+		collection: C,
+		id: E
+	): E extends keyof DataEntryMap[C]
+		? Promise<DataEntryMap[C][E]>
+		: Promise<CollectionEntry<C> | undefined>;
+
+	/** Resolve an array of entry references from the same collection */
+	export function getEntries<C extends keyof ContentEntryMap>(
+		entries: {
+			collection: C;
+			slug: ValidContentEntrySlug<C>;
+		}[]
+	): Promise<CollectionEntry<C>[]>;
+	export function getEntries<C extends keyof DataEntryMap>(
+		entries: {
+			collection: C;
+			id: keyof DataEntryMap[C];
+		}[]
+	): Promise<CollectionEntry<C>[]>;
+
+	export function reference<C extends keyof AnyEntryMap>(
+		collection: C
+	): import('astro/zod').ZodEffects<
+		import('astro/zod').ZodString,
+		C extends keyof ContentEntryMap
+			? {
+					collection: C;
+					slug: ValidContentEntrySlug<C>;
+			  }
+			: {
+					collection: C;
+					id: keyof DataEntryMap[C];
+			  }
+	>;
+	// Allow generic `string` to avoid excessive type errors in the config
+	// if `dev` is not running to update as you edit.
+	// Invalid collection names will be caught at build time.
+	export function reference<C extends string>(
+		collection: C
+	): import('astro/zod').ZodEffects<import('astro/zod').ZodString, never>;
+
 	type ReturnTypeOrOriginal<T> = T extends (...args: any[]) => infer R ? R : T;
-	type InferEntrySchema<C extends keyof typeof entryMap> = import('astro/zod').infer<
+	type InferEntrySchema<C extends keyof AnyEntryMap> = import('astro/zod').infer<
 		ReturnTypeOrOriginal<Required<ContentConfig['collections'][C]>['schema']>
 	>;
 
-	const entryMap: {
-		"blog": {
-"3-extensiones-para-ahorrar-datos-navegando-en-internet.md": {
-  id: "3-extensiones-para-ahorrar-datos-navegando-en-internet.md",
-  slug: "3-extensiones-para-ahorrar-datos-navegando-en-internet",
-  body: string,
-  collection: "blog",
-  data: InferEntrySchema<"blog">
-} & { render(): Render[".md"] },
-"3-herramientas-para-crear-el-readme-de-tu-perfil.md": {
-  id: "3-herramientas-para-crear-el-readme-de-tu-perfil.md",
-  slug: "3-herramientas-para-crear-el-readme-de-tu-perfil",
-  body: string,
-  collection: "blog",
-  data: InferEntrySchema<"blog">
-} & { render(): Render[".md"] },
-"5-herramientas-diferentes-que-utilizan-IA.md": {
-  id: "5-herramientas-diferentes-que-utilizan-IA.md",
-  slug: "5-herramientas-diferentes-que-utilizan-ia",
-  body: string,
-  collection: "blog",
-  data: InferEntrySchema<"blog">
-} & { render(): Render[".md"] },
-"5-pruebas-que-debes-hacer.md": {
-  id: "5-pruebas-que-debes-hacer.md",
-  slug: "5-pruebas-que-debes-hacer",
-  body: string,
-  collection: "blog",
-  data: InferEntrySchema<"blog">
-} & { render(): Render[".md"] },
-"9-cursos-de-platzi-para.md": {
-  id: "9-cursos-de-platzi-para.md",
-  slug: "9-cursos-de-platzi-para",
-  body: string,
-  collection: "blog",
-  data: InferEntrySchema<"blog">
-} & { render(): Render[".md"] },
-"CuCoders.md": {
-  id: "CuCoders.md",
-  slug: "cucoders",
-  body: string,
-  collection: "blog",
-  data: InferEntrySchema<"blog">
-} & { render(): Render[".md"] },
-"ahorra-datos-convierte-y-descarga.md": {
-  id: "ahorra-datos-convierte-y-descarga.md",
-  slug: "ahorra-datos-convierte-y-descarga",
-  body: string,
-  collection: "blog",
-  data: InferEntrySchema<"blog">
-} & { render(): Render[".md"] },
-"astro-tu-proximo-framework-favorito.md": {
-  id: "astro-tu-proximo-framework-favorito.md",
-  slug: "astro-tu-proximo-framework-favorito",
-  body: string,
-  collection: "blog",
-  data: InferEntrySchema<"blog">
-} & { render(): Render[".md"] },
-"ciberdelitos-en-internet-cuidado-donde-pones-tus-llaves.md": {
-  id: "ciberdelitos-en-internet-cuidado-donde-pones-tus-llaves.md",
-  slug: "ciberdelitos-en-internet-cuidado-donde-pones-tus-llaves",
-  body: string,
-  collection: "blog",
-  data: InferEntrySchema<"blog">
-} & { render(): Render[".md"] },
-"como-crear-tu-logotipo-con.md": {
-  id: "como-crear-tu-logotipo-con.md",
-  slug: "como-crear-tu-logotipo-con",
-  body: string,
-  collection: "blog",
-  data: InferEntrySchema<"blog">
-} & { render(): Render[".md"] },
-"como-generar-imagenes-con-ia.md": {
-  id: "como-generar-imagenes-con-ia.md",
-  slug: "como-generar-imagenes-con-ia",
-  body: string,
-  collection: "blog",
-  data: InferEntrySchema<"blog">
-} & { render(): Render[".md"] },
-"como-hakearon-las-llaves-privadas.md": {
-  id: "como-hakearon-las-llaves-privadas.md",
-  slug: "como-hakearon-las-llaves-privadas",
-  body: string,
-  collection: "blog",
-  data: InferEntrySchema<"blog">
-} & { render(): Render[".md"] },
-"company-of-one.md": {
-  id: "company-of-one.md",
-  slug: "company-of-one",
-  body: string,
-  collection: "blog",
-  data: InferEntrySchema<"blog">
-} & { render(): Render[".md"] },
-"crea-tu-blog-en-la.md": {
-  id: "crea-tu-blog-en-la.md",
-  slug: "crea-tu-blog-en-la",
-  body: string,
-  collection: "blog",
-  data: InferEntrySchema<"blog">
-} & { render(): Render[".md"] },
-"crea-tu-web-personal-en-minutos-y-gratis-con-biolink.md": {
-  id: "crea-tu-web-personal-en-minutos-y-gratis-con-biolink.md",
-  slug: "crea-tu-web-personal-en-minutos-y-gratis-con-biolink",
-  body: string,
-  collection: "blog",
-  data: InferEntrySchema<"blog">
-} & { render(): Render[".md"] },
-"el-mejor-sitio-para-descubrir.md": {
-  id: "el-mejor-sitio-para-descubrir.md",
-  slug: "el-mejor-sitio-para-descubrir",
-  body: string,
-  collection: "blog",
-  data: InferEntrySchema<"blog">
-} & { render(): Render[".md"] },
-"errores-de-terceros-en-aplicaciones-multiplataformas.md": {
-  id: "errores-de-terceros-en-aplicaciones-multiplataformas.md",
-  slug: "errores-de-terceros-en-aplicaciones-multiplataformas",
-  body: string,
-  collection: "blog",
-  data: InferEntrySchema<"blog">
-} & { render(): Render[".md"] },
-"genera-el-nombre-de-tu-marca-o-producto-utilizando-inteligencia-artificial.md": {
-  id: "genera-el-nombre-de-tu-marca-o-producto-utilizando-inteligencia-artificial.md",
-  slug: "genera-el-nombre-de-tu-marca-o-producto-utilizando-inteligencia-artificial",
-  body: string,
-  collection: "blog",
-  data: InferEntrySchema<"blog">
-} & { render(): Render[".md"] },
-"pay-link.md": {
-  id: "pay-link.md",
-  slug: "pay-link",
-  body: string,
-  collection: "blog",
-  data: InferEntrySchema<"blog">
-} & { render(): Render[".md"] },
-"planifica-tu-semana-con-weektodo.md": {
-  id: "planifica-tu-semana-con-weektodo.md",
-  slug: "planifica-tu-semana-con-weektodo",
-  body: string,
-  collection: "blog",
-  data: InferEntrySchema<"blog">
-} & { render(): Render[".md"] },
-"vale-la-pena-crear-una.md": {
-  id: "vale-la-pena-crear-una.md",
-  slug: "vale-la-pena-crear-una",
-  body: string,
-  collection: "blog",
-  data: InferEntrySchema<"blog">
-} & { render(): Render[".md"] },
-"weektodo-2-release.md": {
-  id: "weektodo-2-release.md",
-  slug: "weektodo-2-release",
-  body: string,
-  collection: "blog",
-  data: InferEntrySchema<"blog">
-} & { render(): Render[".md"] },
-},
-"store": {
-"item1.md": {
-  id: "item1.md",
-  slug: "item1",
-  body: string,
-  collection: "store",
-  data: InferEntrySchema<"store">
-} & { render(): Render[".md"] },
-"item2.md": {
-  id: "item2.md",
-  slug: "item2",
-  body: string,
-  collection: "store",
-  data: InferEntrySchema<"store">
-} & { render(): Render[".md"] },
-"item3.md": {
-  id: "item3.md",
-  slug: "item3",
-  body: string,
-  collection: "store",
-  data: InferEntrySchema<"store">
-} & { render(): Render[".md"] },
-},
-
+	type ContentEntryMap = {
+		
 	};
 
-	type ContentConfig = typeof import("../src/content/config");
+	type DataEntryMap = {
+		
+	};
+
+	type AnyEntryMap = ContentEntryMap & DataEntryMap;
+
+	type ContentConfig = never;
 }
